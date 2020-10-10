b15036408b2b1de7dcdae54f823f3fac04f9babe
Samuel Martins <samuel.debabomartins@student.supsi.ch>
2020-10-10 13:19:53 +0200

    ex 4 complete

--- ex04/main.c
+++ ex04/main.c
@@ -5,7 +5,7 @@
 #define save_context(CONTEXT) setjmp(CONTEXT)
 #define restore_context(CONTEXT) longjmp(CONTEXT, 1)
 
-typedef enum { __BTHREAD_UNINITIALIZED, __BTHREAD_READY } bthread_state;
+typedef enum { __BTHREAD_UNINITIALIZED, __BTHREAD_READY, __BTHREAD_DEAD } bthread_state;
 typedef void *(*bthread_routine) (void *);
 
 void create_cushion_and_call(bthread_routine fn, bthread_state* state);
@@ -14,11 +14,19 @@ void* bthread2(void* arg);
 void* bthread3(void* arg);
 void* bthread4(void* arg);
 
-jmp_buf main_buf, bthread1_buf, bthread2_buf, bthread3_buf, bthread4_buf;
-bthread_state bthread1_state = __BTHREAD_UNINITIALIZED;
-bthread_state bthread2_state = __BTHREAD_UNINITIALIZED;
-bthread_state bthread3_state = __BTHREAD_UNINITIALIZED;
-bthread_state bthread4_state = __BTHREAD_UNINITIALIZED;
+typedef struct bthread {
+    jmp_buf buf;
+    bthread_state state;
+} Bthread;
+
+jmp_buf main_buf;
+Bthread bthreads[4];
+
+void initialize_threads() {
+    for (int i = 0; i < 4; ++i) {
+        bthreads[i].state = __BTHREAD_UNINITIALIZED;
+    }
+}
 
 void create_cushion_and_call(bthread_routine fn, bthread_state* state)
 {
@@ -28,20 +36,32 @@ void create_cushion_and_call(bthread_routine fn, bthread_state* state)
     fn(NULL);
 }
 
+jmp_buf *next_bthread(int index)
+{
+    for (int i = 1; i <= 4; ++i) {
+        int next_index = (index + i) % 4;
+        if(bthreads[next_index].state != __BTHREAD_DEAD)
+            return &bthreads[next_index].buf;
+    }
+    return &main_buf;
+}
+
 void* bthread1(void* arg)
 {
     volatile int i;
     for(i=0;i<10000;i++) {
         printf("BThread1, i=%d\n", i);
         /* Yield to next bthread */
-        if (!save_context(bthread1_buf)) {
-            if (bthread2_state == __BTHREAD_UNINITIALIZED) {
-                create_cushion_and_call(bthread2, &bthread2_state);
+        if (!save_context(bthreads[0].buf)) {
+            if (bthreads[1].state == __BTHREAD_UNINITIALIZED) {
+                create_cushion_and_call(bthread2, &bthreads[1].state);
             } else {
-                restore_context(bthread2_buf);
+                restore_context(*next_bthread(0));
             }
         }
     }
+    bthreads[0].state = __BTHREAD_DEAD;
+    restore_context(*next_bthread(0));
 }
 
 void* bthread2(void* arg)
@@ -52,14 +72,16 @@ void* bthread2(void* arg)
         printf("BThread2, i=%d\n", i);
 
         /* Yield to next bthread */
-        if (!save_context(bthread2_buf)) {
-            if (bthread3_state == __BTHREAD_UNINITIALIZED) {
-                create_cushion_and_call(bthread3, &bthread3_state);
+        if (!save_context(bthreads[1].buf)) {
+            if (bthreads[2].state == __BTHREAD_UNINITIALIZED) {
+                create_cushion_and_call(bthread3, &bthreads[2].state);
             } else {
-                restore_context(bthread3_buf);
+                restore_context(*next_bthread(1));
             }
         }
     }
+    bthreads[1].state = __BTHREAD_DEAD;
+    restore_context(*next_bthread(1));
 }
 
 void* bthread3(void* arg)
@@ -69,16 +91,17 @@ void* bthread3(void* arg)
         printf("BThread3, i=%d\n", i);
 
         /* Yield to next bthread */
-        if (!save_context(bthread3_buf)) {
+        if (!save_context(bthreads[2].buf)) {
         // We assume that bthread1 is already initialized
-            if (bthread4_state == __BTHREAD_UNINITIALIZED) {
-                create_cushion_and_call(bthread4, &bthread4_state);
+            if (bthreads[3].state == __BTHREAD_UNINITIALIZED) {
+                create_cushion_and_call(bthread4, &bthreads[3].state);
             } else {
-                restore_context(bthread4_buf);
+                restore_context(*next_bthread(2));
             }
         }
     }
-    restore_context(main_buf);
+    bthreads[2].state = __BTHREAD_DEAD;
+    restore_context(*next_bthread(2));
 }
 
 void* bthread4(void* arg)
@@ -89,19 +112,22 @@ void* bthread4(void* arg)
 
         if(i % 5 == 0) {
             /* Yield to next bthread */
-            if (!save_context(bthread4_buf)) {
+            if (!save_context(bthreads[3].buf)) {
                 // We assume that bthread1 is already initialized
-                restore_context(bthread1_buf);
+                restore_context(*next_bthread(3));
             }
         }
 
     }
+    bthreads[3].state = __BTHREAD_DEAD;
+    restore_context(*next_bthread(3));
 }
 
-void main()
+int main()
 {
+    initialize_threads();
    if (!save_context(main_buf)) {
-       create_cushion_and_call(bthread1, &bthread1_state);
+       create_cushion_and_call(bthread1, &bthreads[0].state);
     }
-
+    return 0;
 }
