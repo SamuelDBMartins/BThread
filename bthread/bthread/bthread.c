174f2ec32e980ecb6ca94453c2e1bf21a6c0f6a0
Samuel Martins <tech1@smartins.ch>
2020-11-06 23:20:22 +0100

    ex 10

--- bthread/bthread/bthread.c
+++ bthread/bthread/bthread.c
@@ -5,29 +5,57 @@
 #include "bthread.h"
 #include "bthread_private.h"
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <sys/time.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define STACK_SIZE (1 << 16)
 #define save_context(CONTEXT) sigsetjmp(CONTEXT, 1)
 #define restore_context(CONTEXT) siglongjmp(CONTEXT, 1)
+#define QUANTUM_USEC 100
 
-__bthread_scheduler_private scheduler = {0};
+__bthread_scheduler_private mainScheduler = {0};
 bthread_t cnt = 0;
 
-double get_current_time_millis()
-{
+double get_current_time_millis() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ;
+    return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
 }
 
-__bthread_scheduler_private* bthread_get_scheduler() {
-    return &scheduler;
+void bthread_block_timer_signal() {
+    sigset_t sigsetNew;
+    sigemptyset(&sigsetNew);
+    sigaddset(&sigsetNew, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &sigsetNew, NULL);
+}
+
+void bthread_unblock_timer_signal() {
+    sigset_t sigsetNew;
+    sigemptyset(&sigsetNew);
+    sigaddset(&sigsetNew, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &sigsetNew, NULL);
+}
+
+void bthread_printf(const char *format, ...) // requires stdlib.h and stdarg.h
+{
+    bthread_block_timer_signal();
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    bthread_unblock_timer_signal();
+}
+
+__bthread_scheduler_private *bthread_get_scheduler() {
+    return &mainScheduler;
 }
 
 int bthread_create(bthread_t *bthread, const bthread_attr_t *attr,
-                   void *(*start_routine) (void *), void *arg) {
+                   void *(*start_routine)(void *), void *arg) {
     __bthread_private *bthread_private = malloc(sizeof(__bthread_private));
     bthread_private->attr = *attr;
     bthread_private->body = start_routine;
@@ -39,11 +67,14 @@ int bthread_create(bthread_t *bthread, const bthread_attr_t *attr,
     return tqueue_enqueue(&bthread_get_scheduler()->queue, bthread_private);
 }
 
+
 void bthread_yield() {
     __bthread_private *thread = tqueue_get_data(bthread_get_scheduler()->current_item);
+    bthread_block_timer_signal();
     if (!save_context(thread->context)) {
         restore_context(bthread_get_scheduler()->context);
     }
+    bthread_unblock_timer_signal();
 }
 
 void bthread_exit(void *retval) {
@@ -66,12 +97,11 @@ static int bthread_check_if_zombie(bthread_t bthread, void **retval) {
     }
 
     free(b->stack);
-    if(b == tqueue_get_data(bthread_get_scheduler()->queue)) {
+    if (b == tqueue_get_data(bthread_get_scheduler()->queue)) {
         tqueue_pop(&bthread_get_scheduler()->queue);
     } else {
         tqueue_pop(&node);
     }
-
     return 1;
 }
 
@@ -83,14 +113,29 @@ static TQueue bthread_get_queue_at(bthread_t bthread) {
         return NULL;
 
     do {
-        actual_node = tqueue_at_offset(actual_node, 1);
         __bthread_private *b = tqueue_get_data(actual_node);
         if (b->tid == bthread) {
             return actual_node;
         }
-    }while(actual_node != q);
-
+        actual_node = tqueue_at_offset(actual_node, 1);
+    } while (actual_node != q);
     return NULL;
+}
+
+
+static void bthread_setup_timer() {
+    static bool initialized = false;
+
+    if (!initialized) {
+        signal(SIGVTALRM, (void (*)()) bthread_yield);
+        struct itimerval time;
+        time.it_interval.tv_sec = 0;
+        time.it_interval.tv_usec = QUANTUM_USEC;
+        time.it_value.tv_sec = 0;
+        time.it_value.tv_usec = QUANTUM_USEC;
+        initialized = true;
+        setitimer(ITIMER_VIRTUAL, &time, NULL);
+    }
 }
 
 int bthread_join(bthread_t bthread, void **retval) {
@@ -103,8 +148,8 @@ int bthread_join(bthread_t bthread, void **retval) {
         scheduler->current_item = tqueue_at_offset(scheduler->current_item, 1);
         tp = (__bthread_private *) tqueue_get_data(scheduler->current_item);
 
-        if(tp->state == __BTHREAD_SLEEPING && get_current_time_millis() > tp->wake_up_time) {
-                tp->state = __BTHREAD_READY;
+        if (tp->state == __BTHREAD_SLEEPING && get_current_time_millis() > tp->wake_up_time) {
+            tp->state = __BTHREAD_READY;
         }
     } while (tp->state != __BTHREAD_READY);
     if (tp->stack) {
@@ -118,10 +163,11 @@ int bthread_join(bthread_t bthread, void **retval) {
         target -= (target % 16);
 #endif
 #if __x86_64__
-        asm __volatile__("movq %0, %%rsp" :: "r"((intptr_t) target));
+        asm __volatile__("movq %0, %%rsp"::"r"((intptr_t) target));
 #else
         asm __volatile__("movl %0, %%esp" :: "r"((intptr_t) target));
 #endif
+        bthread_setup_timer();
         bthread_exit(tp->body(tp->arg));
     }
 }
@@ -142,8 +188,9 @@ int bthread_cancel(bthread_t bthread) {
 }
 
 void bthread_testcancel() {
-    TQueue node = bthread_get_scheduler();
+    TQueue node = bthread_get_scheduler()->current_item;
     __bthread_private *thread = tqueue_get_data(node);
     if (thread->cancel_req)
         bthread_exit(NULL);
 }
+
--- bthread/main.c
+++ bthread/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "bthread/bthread.h"
+// gcc -o esempio esempio.c bthread.o schedulers.o tbarrier.o tcondition.o thelper.o tmutex.o tqueue.o tsemaphore.o
+
+void* mythread(void* arg) {
+    int cnt = 0;
+    for (int i = 0; i < 10000; ++i) {
+        cnt++;
+        bthread_sleep((rand() / RAND_MAX) + 0.5);
+    }
+    return (void *) cnt;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    bthread_t t1, t2;
+    int i1, i2;
+    bthread_create(&t1, NULL, mythread, NULL);
+    bthread_create(&t2, NULL, mythread, NULL);
+    bthread_join(t1, &i1);
+    bthread_join(t2, &i2);
+    printf("%d %d", i1, i2);
     return 0;
 }
+
